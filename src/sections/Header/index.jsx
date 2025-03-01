efc4d72bedc8960ce855537587026aa83db86db4
TitaniumSalmon <perapon.s@ku.th>
2025-03-01 21:44:46 +0700

    solarsystem

--- src/sections/Header/index.jsx
+++ src/sections/Header/index.jsx
@@ -4,7 +4,7 @@ import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
 const Header = () => {
     return (
         <div className='flex flex-col gap-2'>
-            <div className='text-left text-3xl text-primaryTitle font-semibold'>Suriya Pinitkan</div>
+            <div className='text-left text-3xl text-primaryTitle font-semibold'>Suriya Solarsystem</div>
             <div className='text-left text-primaryAccent font-semibold'>Software Developer · between jobs</div>
             <div className='text-left text-sm w-5/6'>From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.</div>
             <div className='text-left mt-4'>
